821bfb3a8d00f8514b654a622acb71122ae98c5d
Samson Cain <samsonc99@gmail.com>
2022-05-11 09:28:09 -0500

    rm nvim-old

--- .config/nvim-old/lua/functions.lua
+++
@@ -1,6 +0,0 @@
--- Custom Folds, make them look better
-vim.cmd([[
-  function! CustomFold()
-    return printf('   %-6d%s', v:foldend - v:foldstart + 1, getline(v:foldstart))
-  endfunction
-]])
--- .config/nvim-old/run.sh
+++
@@ -1,9 +0,0 @@
-declare -r NVIM_DIR="${NVIM_DIR:-"$(which nvim)"}"
-
-function install() {
-    "$NVIM_DIR" --headless -u installation_config.lua \
-    -c 'autocmd User PackerComplete quitall' \
-    -c 'PackerSync'
-}
-
-install
